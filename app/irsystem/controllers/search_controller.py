a6e2f2392e5d179ace374c8a3bdae1f435d94eee
oregano345 <adhuang19@gmail.com>
2020-04-03 00:36:15 -0500

    Editing template variables to inclued project name and net ids

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,10 +1,9 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
-
+project_name = "Legal ProTips"
+net_id = "Andrei Kozyrev: aak85, Kevin Ye: ky242, David Huang: dh558, Lily Pagan: lp398, Keivan Shahida: ks963"
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
@@ -15,6 +14,3 @@ def search():
 		output_message = "Your search: " + query
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
