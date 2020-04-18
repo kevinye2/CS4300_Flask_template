55973808e4378a6230a6c753205d99a2ea72510c
oregano345 <adhuang19@gmail.com>
2020-04-17 21:55:13 -0500

    Adding multiprocessing locks

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,25 +1,30 @@
+from multiprocessing import Lock
 from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 from app.irsystem.models.search import *
 
+mutex = Lock()
 @irsystem.route('/', methods=['GET'])
 def initializeTemplate():
-	return render_template('search.html')
+	with mutex:
+		return render_template('search.html')
 
 @irsystem.route('/postquery', methods=['POST'])
 def handleQuery():
-	query = request.json.get('query')
-	county = request.json.get('county')
-	resp_obj = legalTipResp(query, county)
-	return Response(response=json.dumps(resp_obj), status=200,
-		content_type='application/json')
+	with mutex:
+		query = request.json.get('query')
+		county = request.json.get('county')
+		resp_obj = legalTipResp(query, county)
+		return Response(response=json.dumps(resp_obj), status=200,
+			content_type='application/json')
 
 @irsystem.route('/postfeedback', methods=['POST'])
 def handleFeedback():
-	query = request.json.get('query')
-	county = request.json.get('county')
-	relevant_doc_id = request.json.get('relevant_rating')
-	resp_obj = feedbackRatings(query, county, relevant_doc_id)
-	return Response(response=json.dumps(resp_obj), status=200,
-		content_type='application/json')
+	with mutex:
+		query = request.json.get('query')
+		county = request.json.get('county')
+		relevant_doc_id = request.json.get('relevant_rating')
+		resp_obj = feedbackRatings(query, county, relevant_doc_id)
+		return Response(response=json.dumps(resp_obj), status=200,
+			content_type='application/json')
